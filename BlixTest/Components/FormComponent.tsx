2b62ef9b94e8a7091dfb19704193c98a2f43af0a
madomaga <marcin.domagala@capgemini.com>
2023-12-05 14:36:39 +0100

    forms with validation

--- BlixTest/Components/FormComponent.tsx
+++ BlixTest/Components/FormComponent.tsx
@@ -1,60 +1,101 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { View, StyleSheet, Text } from "react-native";
 import InputWithLeftTextComponent from "./InputWithLeftTextComponent";
 import { Button, Checkbox } from "react-native-paper";
 
 const FormComponent = () => {
-  const [checked, setChecked] = useState<boolean>(false);
-
+  const [accountType, setAccountType] = useState<string>("");
+  const [userName, setUserName] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [serverAddress, setServerAddress] = useState<string>("");
+  const [serverPath, setServerPath] = useState<string>("");
+  const [port, setPort] = useState<string>("");
+  const [useSSL, setUseSSL] = useState<boolean>(false);
+  const handlePress = () => {
+    if (accountType === "Manual") {
+      if (
+        !accountType ||
+        !userName ||
+        !password ||
+        !serverAddress ||
+        !serverPath ||
+        !port
+      ) {
+        alert("Please fill in all fields");
+        return;
+      }
+    } else {
+      if (!accountType || !userName || !password || !serverAddress) {
+        alert("Please fill in all fields");
+        return;
+      }
+    }
+  };
   return (
     <View style={styles.outerContainer}>
       <InputWithLeftTextComponent
         inputType="list"
         label="Account Type"
         maxLength={20}
+        selectedValue={accountType}
+        onSelectValue={setAccountType}
       />
       <InputWithLeftTextComponent
         inputType="textInput"
         label="User Name"
         maxLength={255}
+        value={userName}
+        onChangeText={setUserName}
       />
       <InputWithLeftTextComponent
         inputType="textInput"
         label="Password"
         maxLength={50}
         secureText={true}
+        value={password}
+        onChangeText={setPassword}
       />
       <InputWithLeftTextComponent
         inputType="textInput"
         label="Server Address"
         maxLength={255}
+        value={serverAddress}
+        onChangeText={setServerAddress}
       />
-      <InputWithLeftTextComponent
-        inputType="textInput"
-        label="Server Path"
-        maxLength={255}
-      />
-      <InputWithLeftTextComponent
-        inputType="textInput"
-        label="Port"
-        maxLength={4}
-      />
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          gap: 4,
-          alignSelf: "flex-end",
-        }}
-      >
-        <Text>Use SSL</Text>
-        <Checkbox
-          status={checked ? "checked" : "unchecked"}
-          onPress={() => setChecked(!checked)}
-        />
-      </View>
+      {accountType === "Advanced" && (
+        <View>
+          <InputWithLeftTextComponent
+            inputType="textInput"
+            label="Server Path"
+            maxLength={255}
+            value={serverPath}
+            onChangeText={setServerPath}
+          />
+          <InputWithLeftTextComponent
+            inputType="textInput"
+            label="Port"
+            maxLength={4}
+            value={port}
+            onChangeText={setPort}
+          />
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 4,
+              alignSelf: "flex-end",
+            }}
+          >
+            <Text>Use SSL</Text>
+            <Checkbox
+              status={useSSL ? "checked" : "unchecked"}
+              onPress={() => setUseSSL(!useSSL)}
+            />
+          </View>
+        </View>
+      )}
 
-      <Button mode="contained" onPress={() => console.log("Pressed")}>
+      <Button mode="contained" onPress={handlePress}>
         Press me
       </Button>
     </View>
--- BlixTest/Components/InputWithLeftTextComponent.tsx
+++ BlixTest/Components/InputWithLeftTextComponent.tsx
@@ -1,11 +1,6 @@
 import { useState } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  GestureResponderEvent,
-  LayoutAnimation,
-} from "react-native";
+import { Dispatch, SetStateAction } from "react";
+import { LayoutAnimation, View } from "react-native";
 import { List, TextInput } from "react-native-paper";
 
 interface TextInputInterface {
@@ -13,33 +8,43 @@ interface TextInputInterface {
   label: string;
   maxLength: number;
   secureText?: boolean;
-  //   serverAddress: string;
-  //   serverPath?: string;
-  //   port?: number;
 }
+
+interface ListInputInterface extends TextInputInterface {
+  selectedValue?: string;
+  onSelectValue?: Dispatch<SetStateAction<string>>;
+  value?: string;
+  onChangeText?: Dispatch<SetStateAction<string>>;
+}
+
 function InputWithLeftTextComponent({
   inputType,
   label,
   maxLength,
   secureText,
-}: TextInputInterface) {
-  const [selectedItem, setSelectedItem] = useState<string>("");
+  selectedValue,
+  onSelectValue,
+  value,
+  onChangeText,
+}: ListInputInterface) {
   const [expanded, setExpanded] = useState(false);
+
   const handlePress = () => {
     setExpanded(!expanded);
     LayoutAnimation.easeInEaseOut();
   };
+
   const handleItemPress = (item: string) => {
-    setSelectedItem(item);
+    onSelectValue && onSelectValue(item);
     setExpanded(false);
     LayoutAnimation.easeInEaseOut();
   };
 
   return (
     <View>
-      {inputType === "list" ? (
+      {inputType === "list" && (
         <List.Accordion
-          title={selectedItem ? selectedItem : label}
+          title={selectedValue || label}
           expanded={expanded}
           onPress={handlePress}
           key={"Account Type"}
@@ -50,23 +55,21 @@ function InputWithLeftTextComponent({
           />
           <List.Item title="Manual" onPress={() => handleItemPress("Manual")} />
         </List.Accordion>
-      ) : inputType === "textInput" ? (
+      )}
+      {inputType === "textInput" && (
         <TextInput
           label={label}
           maxLength={maxLength}
           contentStyle={{ backgroundColor: "#fff" }}
           secureTextEntry={secureText}
+          value={value || (selectedValue as string)}
+          onChangeText={
+            onChangeText || (onSelectValue as Dispatch<SetStateAction<string>>)
+          }
         />
-      ) : null}
+      )}
     </View>
   );
 }
 
 export default InputWithLeftTextComponent;
-const styles = StyleSheet.create({
-  inputText: {
-    padding: 4,
-    marginLeft: 4,
-    width: "100%",
-  },
-});
