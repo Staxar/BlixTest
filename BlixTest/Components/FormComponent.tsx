6273f23729df11c76651ff95d434c6a0f4ed5e39
madomaga <marcin.domagala@capgemini.com>
2023-12-05 13:06:59 +0100

    init app

---
+++ BlixTest/Components/FormComponent.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { View, Text } from "react-native";
+
+const FormComponent = () => {
+  return (
+    <View>
+      <Text>FormComponent</Text>
+    </View>
+  );
+};
+
+export default FormComponent;
